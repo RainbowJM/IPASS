330e36482ef313311128c554bfd218d75305c8ad
RainbowJM <Jevona98@live.com>
2020-06-28 21:30:47 +0200

    Made a REST for adding availability

--- src/main/java/project/ipass/application/webservices/AvailabityResource.java
+++ src/main/java/project/ipass/application/webservices/AvailabityResource.java
@@ -1,18 +1,36 @@
 package project.ipass.application.webservices;
 
+import project.ipass.application.model.Appointment;
+import project.ipass.application.model.Availability;
+
+import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.AbstractMap;
+import java.util.Date;
 
 @Path("/availabity")
 public class AvailabityResource {
 
     @POST
     @Produces(MediaType.APPLICATION_JSON)
-    public Response createAvailabity(){
-        return null;
+    public Response createAvailabity(@FormParam("from")String dateF,
+                                     @FormParam("till")String dateT,
+                                     @FormParam("description")String desc){
+        Date df = Appointment.parseDate(dateF);
+        Date dt = Appointment.parseDate(dateT);
+        for (Availability avai : Availability.getAllAvailability()){
+            if (avai.getFrom().equals(dateF) && avai.getTill().equals(dateT)){
+                return Response
+                        .status(Response.Status.CONFLICT)
+                        .entity(new AbstractMap.SimpleEntry<String, String>("Error", "The time period is already booked"))
+                        .build();
+            }
+        }
+        return Response.ok(new Availability(df, dt,desc)).build();
     }
 
 }
